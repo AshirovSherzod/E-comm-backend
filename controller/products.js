d6236b57e0ccde333534ee5ba458fd660ed77797
AshirovSherzod <ashirovsherzod614@gmail.com>
2024-08-19 12:17:12 +0500

    task done

--- controller/products.js
+++ controller/products.js
@@ -1,223 +1,227 @@
-import { Products, validateProducts } from "../models/productSchema.js"
+import path from "path";
+import { Products, validateProducts } from "../models/productSchema.js";
+import fs from "fs";
 
 class ProductsController {
-    async get(req, res) {
-        try {
+  async get(req, res) {
+    try {
+      let { limit, skip } = req.query;
+      const products = await Products.find()
+        .populate([
+          { path: "adminId", select: ["fname", "username"] },
+          { path: "categoryId", select: ["title"] },
+        ])
+        .sort({ createdAt: -1 })
+        .limit(limit)
+        .skip(limit * skip);
 
-            let { limit, skip } = req.query
-            const products = await Products.find().populate([
-                { path: "adminId", select: ["fname", "username"] },
-                { path: "categoryId", select: ["title"] }
-            ]).sort({ createdAt: -1 }).limit(limit).skip(limit * skip)
-
-            if (!products.length) {
-                return res.status(400).json({
-                    msg: "Blog is not defined",
-                    variant: "error",
-                    payload: null
-                })
-            }
-            res.status(200).json({
-                msg: "All Blogs",
-                variant: "success",
-                payload: products,
-                totalCount: products.length
-            })
-        } catch (error) {
-
-            res.status(500).json({
-                msg: "Server error",
-                variant: "error",
-                payload: null
-            })
-        }
+      if (!products.length) {
+        return res.status(400).json({
+          msg: "Blog is not defined",
+          variant: "error",
+          payload: null,
+        });
+      }
+      res.status(200).json({
+        msg: "All Blogs",
+        variant: "success",
+        payload: products,
+        totalCount: products.length,
+      });
+    } catch (error) {
+      res.status(500).json({
+        msg: "Server error",
+        variant: "error",
+        payload: null,
+      });
     }
-    async getFromCategory(req, res) {
-        try {
-            let { categoryId } = req.params
-            console.log(categoryId);
+  }
+  async getFromCategory(req, res) {
+    try {
+      let { categoryId } = req.params;
 
-            const products = await Products.find({ categoryId }).populate([
-                { path: "adminId", select: ["fname", "username"] },
-                { path: "categoryId", select: ["title"] }
-            ]).sort({ createdAt: -1 })
+      const products = await Products.find({ categoryId })
+        .populate([
+          { path: "adminId", select: ["fname", "username"] },
+          { path: "categoryId", select: ["title"] },
+        ])
+        .sort({ createdAt: -1 });
 
-            if (!products.length) {
-                return res.status(400).json({
-                    msg: "Products is not defined",
-                    variant: "error",
-                    payload: null
-                })
-            }
-            res.status(200).json({
-                msg: "All Blogs",
-                variant: "success",
-                payload: products,
-                totalCount: products.length
-            })
-        } catch (error) {
-
-            res.status(500).json({
-                msg: "Server error",
-                variant: "error",
-                payload: null
-            })
-        }
+      if (!products.length) {
+        return res.status(400).json({
+          msg: "Products is not defined",
+          variant: "error",
+          payload: null,
+        });
+      }
+      res.status(200).json({
+        msg: "All Blogs",
+        variant: "success",
+        payload: products,
+        totalCount: products.length,
+      });
+    } catch (error) {
+      res.status(500).json({
+        msg: "Server error",
+        variant: "error",
+        payload: null,
+      });
     }
-    async create(req, res) {
-        try {
+  }
+  async create(req, res) {
+    try {
+      let urls = req.files.map(
+        (i) => `${req.protocol}://${req.get("host")}/images/${i.filename}`
+      );
 
-            let urls = req.files.map(i => `${req.protocol}://${req.get("host")}/images/${i.filename}`)
-            let { title, price, oldPrice, stock, rating, views, units, desc, info, available, categoryId } = req.body
+      let {
+        title,
+        price,
+        oldPrice,
+        stock,
+        rating,
+        views,
+        units,
+        desc,
+        info,
+        available,
+        categoryId,
+      } = req.body;
 
-            const existingProduct = await Products.findOne({ title });
-            if (existingProduct) {
-                return res.status(400).json({
-                    msg: "Product already exists.",
-                    variant: "error",
-                    payload: null,
-                });
-            }
+      const existingProduct = await Products.findOne({ title });
+      if (existingProduct) {
+        return res.status(400).json({
+          msg: "Product already exists.",
+          variant: "error",
+          payload: null,
+        });
+      }
 
-            let newProduct = {
-                title,
-                price,
-                oldPrice,
-                stock,
-                rating,
-                views,
-                categoryId,
-                units,
-                desc,
-                urls,
-                info,
-                available,
-            }
+      let newProduct = {
+        title,
+        price,
+        oldPrice,
+        stock,
+        rating,
+        views,
+        categoryId,
+        units,
+        desc,
+        urls,
+        info,
+        available,
+      };
 
-            const { error } = validateProducts(newProduct)
+      const { error } = validateProducts(newProduct);
 
-            if (error) {
-                return res.status(400).json({
-                    msg: error.details[0].message,
-                    variant: "warning",
-                    payload: null
-                })
-            }
-            const products = await Products.create({ ...newProduct, adminId: req.admin._id })
-            res.status(201).json({
-                msg: "Category is created",
-                variant: "success",
-                payload: products
-            })
-        } catch {
-            res.status(500).json({
-                msg: "Server error",
-                variant: "error",
-                payload: null
-            })
-        }
+      if (error) {
+        return res.status(400).json({
+          msg: error.details[0].message,
+          variant: "warning",
+          payload: null,
+        });
+      }
+      const products = await Products.create({
+        ...newProduct,
+        adminId: req.admin._id,
+      });
+      res.status(201).json({
+        msg: "Category is created",
+        variant: "success",
+        payload: products,
+      });
+    } catch {
+      res.status(500).json({
+        msg: "Server error",
+        variant: "error",
+        payload: null,
+      });
     }
-    async delete(req, res) {
-        try {
-            const { id } = req.params
-            const existCategory = await Products.findById(id)
-            if (!existCategory) {
-                return res.status(400).json({
-                    msg: "Product is not found",
-                    variant: "warning",
-                    payload: null
-                })
-            }
-            const category = await Products.findByIdAndDelete(id, { new: true })
+  }
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      const existCategory = await Products.findById(id);
+      if (!existCategory) {
+        return res.status(400).json({
+          msg: "Product is not found",
+          variant: "warning",
+          payload: null,
+        });
+      }
 
-            res.status(200).json({
-                msg: "Product delete successfully",
-                variant: "success",
-                payload: category
-            })
-        } catch {
-            res.status(500).json({
-                msg: "Server error",
-                variant: "error",
-                payload: null
-            })
-        }
+      const category = await Products.findByIdAndDelete(id, { new: true });
+      res.status(200).json({
+        msg: "Product delete successfully",
+        variant: "success",
+        payload: category,
+      });
+    } catch {
+      res.status(500).json({
+        msg: "Server error",
+        variant: "error",
+        payload: null,
+      });
     }
-    async update(req, res) {
-        try {
-            const { id } = req.params
+  }
+  async update(req, res) {
+    try {
+      const { id } = req.params;
 
-            if (req.body.urls) {
-                return res.status(400).json({
-                    msg: "The images cannot be edited here",
-                    variant: "warning",
-                    payload: null
-                })
-            }
+      if (req.body.urls) {
+        return res.status(400).json({
+          msg: "The images cannot be edited here",
+          variant: "warning",
+          payload: null,
+        });
+      }
 
-            const existProducts = Products.findById(id)
-            if (!existProducts) {
-                return res.status(400).json({
-                    msg: "Product is not found",
-                    variant: "warning",
-                    payload: null
-                })
-            }
+      const existProducts = Products.findById(id);
+      if (!existProducts) {
+        return res.status(400).json({
+          msg: "Product is not found",
+          variant: "warning",
+          payload: null,
+        });
+      }
 
-            const user = await Products.findByIdAndUpdate(id, req.body, { new: true })
-            res.status(200).json({
-                msg: "Products updated successfully",
-                variant: "success",
-                payload: user
-            })
-        } catch {
-            res.status(500).json({
-                msg: "Server error",
-                variant: "error",
-                payload: null
-            })
-        }
+      const user = await Products.findByIdAndUpdate(id, req.body, {
+        new: true,
+      });
+      res.status(200).json({
+        msg: "Products updated successfully",
+        variant: "success",
+        payload: user,
+      });
+    } catch {
+      res.status(500).json({
+        msg: "Server error",
+        variant: "error",
+        payload: null,
+      });
     }
-    async updateImages(req, res) {
-        try {
-            const { id } = req.params
-            let { title, price, oldPrice, stock, rating, views, units, desc, info, available, categoryId, urls } = req.body
+  }
+  async deleteImages(req, res) {
+    try {
+      let { id } = req.params;
+      let product = await Products.findById(id);
 
+      product?.urls?.forEach((el) => {
+        let name = el.split("/").slice(-1)[0];
+        const filePath = path.join("files", name);
+        fs.unlinkSync(filePath);
+      });
 
-            if (!title && !price && !oldPrice && !stock && !rating && !views && !units && !desc && !info && !available && !categoryId) {
-
-                const existProducts = Products.findById(id)
-                if (!existProducts) {
-                    return res.status(400).json({
-                        msg: "Product is not found",
-                        variant: "warning",
-                        payload: null
-                    })
-                }
-
-                const user = await Products.findByIdAndUpdate(id, urls, { new: true })
-                res.status(200).json({
-                    msg: "Products updated successfully",
-                    variant: "success",
-                    payload: user
-                })
-            }
-            else {
-                return res.status(400).json({
-                    msg: "Only images can be edited here",
-                    variant: "warning",
-                    payload: null
-                })
-            }
-
-        } catch {
-            res.status(500).json({
-                msg: "Server error",
-                variant: "error",
-                payload: null
-            })
-        }
+      await Products.findByIdAndDelete(id);
+      res.json("product deleted");
+    } catch {
+      res.status(500).json({
+        msg: "Server error",
+        variant: "error",
+        payload: null,
+      });
     }
+  }
 }
 
-export default new ProductsController()
+export default new ProductsController();
--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,41 @@
-import express from "express"
-import AdminsController from "../controller/admins.js"
-import CategoryController from "../controller/category.js"
-import ProductsController from "../controller/products.js"
-import { Auth, OwnerAuth as OwnerAuth } from "../middleware/adminAuth.js"
-import { upload } from "../middleware/uploader.js"
-const router = express.Router()
+import express from "express";
+import AdminsController from "../controller/admins.js";
+import CategoryController from "../controller/category.js";
+import ProductsController from "../controller/products.js";
+import { Auth, OwnerAuth as OwnerAuth } from "../middleware/adminAuth.js";
+import { upload } from "../middleware/uploader.js";
+const router = express.Router();
 
 //admins
-router.get("/admins", [Auth, OwnerAuth], AdminsController.get)
-router.get("/admins/profile", [Auth], AdminsController.getprofile)
-router.get("/admins/:id", [Auth, OwnerAuth], AdminsController.getOneAdmin)
-router.post("/admins/sign-up", [Auth, OwnerAuth], AdminsController.create)
-router.post("/admins/sign-in", AdminsController.signin)
-router.patch("/admins/:id", [Auth, OwnerAuth], AdminsController.update)
-router.delete("/admins/:id", [Auth, OwnerAuth], AdminsController.delete)
+router.get("/admins", [Auth, OwnerAuth], AdminsController.get);
+router.get("/admins/profile", [Auth], AdminsController.getprofile);
+router.get("/admins/:id", [Auth, OwnerAuth], AdminsController.getOneAdmin);
+router.post("/admins/sign-up", [Auth, OwnerAuth], AdminsController.create);
+router.post("/admins/sign-in", AdminsController.signin);
+router.patch("/admins/:id", [Auth, OwnerAuth], AdminsController.update);
+router.delete("/admins/:id", [Auth, OwnerAuth], AdminsController.delete);
 
-//category 
-router.get("/category", [Auth], CategoryController.get)
-router.post("/category", [Auth], CategoryController.create)
-router.delete("/category/:id", [Auth], CategoryController.delete)
-router.patch("/category/:id", [Auth], CategoryController.update)
+//category
+router.get("/category", [Auth], CategoryController.get);
+router.post("/category", [Auth], CategoryController.create);
+router.delete("/category/:id", [Auth], CategoryController.delete);
+router.patch("/category/:id", [Auth], CategoryController.update);
 
-//products 
-router.get("/products", [Auth], ProductsController.get)
-router.get("/products/category/:categoryId", [Auth], ProductsController.getFromCategory)
-router.post("/products", [Auth, upload.array("photos")], ProductsController.create)
-router.patch("/products/:id", [Auth], ProductsController.update)
-router.patch("/products/images/:id", [Auth], ProductsController.updateImages)
-router.delete("/products/:id", [Auth], ProductsController.delete)
+//products
+router.get("/products", [Auth], ProductsController.get);
+router.get(
+  "/products/category/:categoryId",
+  [Auth],
+  ProductsController.getFromCategory
+);
+router.post(
+  "/products",
+  [Auth, upload.array("photos")],
+  ProductsController.create
+);
+router.patch("/products/:id", [Auth], ProductsController.update);
+router.delete("/products/images/:id", [Auth], ProductsController.deleteImages);
+router.delete("/products/:id", [Auth], ProductsController.deleteImages);
 
-export default router
+export default router;
+
